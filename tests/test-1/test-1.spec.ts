d4dfad5e5607f8a6c6b2acad38ba03df07b1d0c1
Viktor Mikeska <vik.mikeska@gmail.com>
2025-01-10 22:28:29 +0100

    Update for test-1

--- tests/test-1/test-1.spec.ts
+++ tests/test-1/test-1.spec.ts
@@ -3,10 +3,17 @@ import { PerformanceResourceTiming } from "node:perf_hooks";
 
 test("Basic navigation", async ({ page }) => {
   await page.goto("https://gitlab.com");
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(300);
   await page.reload();
 });
 
 test("Interacting with web elements on github", async ({ page }) => {
   await page.goto("https://gitlab.com");
+  await page.click("#onetrust-accept-btn-handler");
+  await page
+    .locator("#be-navigation-desktop")
+    .getByRole("link", { name: "Get free trial" })
+    .click();
+  await page.getByTestId("new-user-first-name-field").fill("John1");
+  await page.getByTestId("new-user-last-name-field").fill("Snow1");
 });
