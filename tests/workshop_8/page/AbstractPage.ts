21e2a4c347b0dae2361f3761b967f6113062ab91
Viktor Mikeska <vik.mikeska@gmail.com>
2025-01-22 20:30:23 +0100

    Delete examples add tests

---
+++ tests/workshop_8/page/AbstractPage.ts
@@ -0,0 +1,11 @@
+import { Page } from '@playwright/test';
+
+export abstract class AbstractPage {
+  protected page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  abstract open(url: string): Promise<void>;
+}
